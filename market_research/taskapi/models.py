57e1b8e4392ec0c77f4902c5102ddf816729b564
LucasSD <lucas.stonedrake@gmail.com>
2021-07-04 19:39:38 +0100

    adjust settings.py and models.py for older Django version. All tests pass

--- market_research/taskapi/models.py
+++ market_research/taskapi/models.py
@@ -5,29 +5,27 @@ from django.db.models.fields import DateField
 class Tile(models.Model):
     """Create a Tile model."""
 
-    class Status(models.IntegerChoices):
-        """Create a Status subclass of Tile model."""
+    STATUS = [
+    (1, 'Live'),
+    (2, 'Pending'),
+    (3, 'Archived')
+  ]
 
-        LIVE = 1, "Live"
-        PENDING = 2, "Pending"
-        ARCHIVED = 3, "Archived"
-
-    status = models.PositiveSmallIntegerField(choices=Status.choices)
+    status = models.PositiveSmallIntegerField(choices=STATUS)
     launch_date = DateField()
 
 
 class Task(models.Model):
     """Create a Task model."""
 
-    class Type(models.IntegerChoices):
-        """Create a Type subclass of Task model."""
-
-        SURVEY = 1, "Survey"
-        DISCUSSION = 2, "Discussion"
-        DIARY = 3, "Diary"
+    TYPES = [
+    (1, 'Survey'),
+    (2, 'Discussion'),
+    (3, 'Diary'),
+  ]
 
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
     order = models.SmallIntegerField()
-    type = models.PositiveSmallIntegerField(choices=Type.choices)
+    type = models.PositiveSmallIntegerField(choices=TYPES, null=True)
     tile = models.ForeignKey(Tile, on_delete=models.CASCADE, null=True)
