da602c27324ae799c4ad712a0e817a29dcc0f9a0
LucasSD <lucas.stonedrake@gmail.com>
2021-07-04 21:04:32 +0100

    add test to tests.py

--- market_research/taskapi/tests.py
+++ market_research/taskapi/tests.py
@@ -6,7 +6,10 @@ from .models import Tile, Task
 class TaskTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_task = Task()
+        test_tile = Tile()
+        cls.test_task = Task(title="test_title", type=1, tile=test_tile)
+
+        cls.test_task_no_tile = Task()
 
     def test_title_max_length(self):
         max_length = self.test_task._meta.get_field("title").max_length
@@ -21,7 +24,11 @@ class TaskTest(TestCase):
         self.assertEqual(choices, [(1, "Survey"), (2, "Discussion"), (3, "Diary")])
 
     def test_tile_null(self):
-        self.assertEqual(self.test_task.tile, None)
+        self.assertEqual(self.test_task_no_tile.tile, None)
+
+    def test_obj_name(self):  # test __str__
+        expected_obj_name = f"{self.test_task.title} {self.test_task.type} {self.test_task.tile}"
+        self.assertEqual(expected_obj_name, str(self.test_task))
 
 
 class TileTest(TestCase):
