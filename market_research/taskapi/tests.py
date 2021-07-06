f7ef4a6f088f6e522b8df93670f3e8cbeb503112
LucasSD <lucas.stonedrake@gmail.com>
2021-07-06 15:14:19 +0100

    add tests for single tiles

--- market_research/taskapi/tests.py
+++ market_research/taskapi/tests.py
@@ -1,12 +1,12 @@
-from django.test import Client, TestCase
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
 
 from .models import Task, Tile
 from .serializers import TileSerializer
 
-client = Client()
-class TaskTest(TestCase):
+client = APIClient()
+class TaskTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
         test_tile = Tile.objects.create(title="Some Tile", status=1)
@@ -35,7 +35,7 @@ class TaskTest(TestCase):
         self.assertEqual(expected_obj_name, str(self.test_task))
 
 
-class TileTest(TestCase):
+class TileTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.test_tile = Tile.objects.create(title="Some Tile", status=1)
@@ -55,24 +55,49 @@ class TileTest(TestCase):
         expected_obj_name = f"{self.test_tile.title} -- ID: {self.test_tile.id}"
         self.assertEqual(expected_obj_name, str(self.test_tile))
 
-class GetAllTilesTest(TestCase):
+class GetAllTilesTest(APITestCase):
     @classmethod
-    def setUpTestDate(cls):
+    def setUpTestData(cls):
         Tile.objects.create(
-        title='Tile A', status=3, launch_date='23/12/2026')
+        title='Tile A', status=3, launch_date='2026-12-23')
         Tile.objects.create(
-        title='Tile B', status=1, launch_date='05/11/2026')
+        title='Tile B', status=1, launch_date='2026-11-05')
         Tile.objects.create(
         title='Tile C', status=2)
         Tile.objects.create(
         title='Tile D', status=1)
 
     def test_get_all_tiles(self):
-        # get API response
-        response = self.client.get(reverse('tiles'))
+        # use default viewset name
+        response = self.client.get(reverse('tile-list'))
         # get data from db
         tiles = Tile.objects.all()
         serializer = TileSerializer(tiles, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class GetSingleTileTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.tileA = Tile.objects.create(
+            title='Tile A', status=3, launch_date='2026-12-23')
+        cls.tileB = Tile.objects.create(
+            title='Tile B', status=1, launch_date='2026-11-05')
+        cls.tileC = Tile.objects.create(
+            title='Tile C', status=2)
+        cls.tileD = Tile.objects.create(
+            title='Tile D', status=2)
+
+    def test_get_valid_single_tile(self):
+        response = client.get(
+            reverse('tile-detail', kwargs={'pk': self.tileA.pk}))
+        tile = Tile.objects.get(pk=self.tileA.pk)
+        serializer = TileSerializer(tile)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_tile(self):
+        response = client.get(
+            reverse('tile-detail', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
